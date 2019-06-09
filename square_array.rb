c731de90761f8517142363180b29a6cb47232677
dennis wang <dwang0816@gmail.com>
2019-06-09 03:17:42 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
   # your code here
-  squared_array = []
-  array.each do |number|
-    squared_array.push(number ** 2)
-  squared_array
+  arr = []
+  array.each do |i|
+   arr.push(i ** 2)
   end
-end
+  return arr
+end 
